d34f00c41a889a204c39bde18777f68856f8ca3b
iamprincesly <iamprincesly@gmail.com>
2022-01-09 16:34:55 +0100

    Amend doc for function type annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,19 +1,60 @@
+/**
+ * @author Sylvanus Etim
+ * @email iamprincesly@gmail.com
+ * @create date 2022-01-09 16:27:36
+ * @modify date 2022-01-09 16:27:36
+ * @desc Functions in TypeScript
+ */
+/**
+ * Function to add two numbers
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}  a + b
+ */
 const add = (a: number, b: number): number => {
     return a + b;
 };
 
+/**
+ * Function to subtract
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number} a - b
+ */
 const subtract = (a: number, b: number) => {
     return a - b;
 };
 
+/**
+ * Function to drive
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}  a / b
+ */
 function divide(a: number, b: number): number {
     return a / b;
 }
 
+/**
+ * Function to multiply numbers
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}  a * b
+ */
 const multiply = (a: number, b: number): number => {
     return a * b;
 };
 
+/**
+ * Function that returns nothing
+ *
+ * @param {string} message
+ * @return {void}
+ */
 const logger = (message: string): void => {
     console.log(message);
 
@@ -22,6 +63,13 @@ const logger = (message: string): void => {
     // return undefined;
 };
 
+/**
+ * Function that returns never
+ * means function that will never return
+ *
+ * @param {string} message
+ * @return {*}  {never}
+ */
 const throwError = (message: string): never => {
     if (!message) {
         throw new Error(message);
@@ -35,13 +83,22 @@ const todaysWeather = {
     weather: 'sunny',
 };
 
-// const logWeather = (forecast: { date: Date; weather: string }): void => {
-//     console.log(forecast.date);
-//     console.log(forecast.weather);
-// };
+/**
+ * Type annotations for function parameters
+ *
+ * @param {{ date: Date; weather: string }} forecast
+ */
+const logWeather1 = (forecast: { date: Date; weather: string }): void => {
+    console.log(forecast.date);
+    console.log(forecast.weather);
+};
 
-// TypeScript destructuring
-const logWeather = ({date, weather}: { date: Date; weather: string }): void => {
+/**
+ * TypeScript destructuring data from object
+ *
+ * @param {{ date: Date; weather: string; }} { date, weather }
+ */
+const logWeather2 = ({ date, weather }: { date: Date; weather: string }): void => {
     console.log(date);
     console.log(weather);
 };
@@ -52,4 +109,5 @@ const logWeather = ({date, weather}: { date: Date; weather: string }): void => {
 //     console.log(weather);
 // };
 
-logWeather(todaysWeather);
+logWeather1(todaysWeather);
+logWeather2(todaysWeather);
